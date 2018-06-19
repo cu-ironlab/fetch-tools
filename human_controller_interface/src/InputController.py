e311f086210d763e955d1f305a9f672ea55d7fd6
Connor Brooks <connormbrooks@gmail.com>
2018-06-19 15:39:36 -0600

    finished keyboard controller

--- human_controller_interface/src/InputController.py
+++ human_controller_interface/src/InputController.py
@@ -11,37 +11,16 @@ class InputController():
 		self.create_control_dict()
 
 	def create_control_dict(self):
-		"""
-		Creates dict that will be updated with control signal inputs
-		Dict filled using specifications given in spec_etree
-
-		TODO: move the following xml reference somewhere else
-		<?xml version="1.0"?>
-		<data>
-			<input name='forward'>
-				<type>float</type>
-			</input>
-			<input name='command'>
-				<type>string</type>
-			</input>
-			.
-			.
-			.
-		</data>
-		"""
 		self.control_dict = {}
 		for input_signal in self.spec_etree.iter('input'):
 			input_name = input_signal.attrib['name']
-			inputs = []
-			input_types = []
-			for x_in in input_signal.iter('type'):
-				input_types.append(x_in.text)
-				inputs.append(None)
-			self.control_dict[input_name]["types"] = input_types
-			self.control_dict[input_name]["inputs"] = inputs
+			x_in = input_signal.find('type')
+			self.control_dict[input_name] = {}
+			self.control_dict[input_name]["type"] = x_in.text
+			self.control_dict[input_name]["input"] = None
 
 	def update_control_signal(self, signal_name, signal_update):
-		self.control_dict[signal_name]["inputs"] = signal_update
+		self.control_dict[signal_name]["input"] = signal_update
 		#check for input type match here? or somewhere else?
 
 	def get_all_controls(self):
